21e2edb8c3c6a540e40dcab5c3ab6b9a26a83653
bluellow <1091853304@qq.com>
2016-08-29 09:40:04 +0800

    初代 Signed-off-by: bluellow <1091853304@qq.com>

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// $("#one").css("background","#bbffaa");  
+// $(".mini").css("background","#bbffaa");
+// $("div").css("background","#bbffaa");
+// $("span,#two").css("background","#bbffaa");
+// $("body > div").css("background","#bbffaa");
+// $("#two + div").css("background","#bbffaa");
+// $("#two ~ div").css("background","#bbffaa");
+// $("#two").next("div").css("background","#bbffaa");
+// $("#two").nextAll("div").css("background","#bbffaa");
+// $("#two").siblings("div").css("background","#bbffaa");
+
+// $("div:even").css("background","#bbffaa");//even包含子辈的标签
+// $("div:eq(1)").css("background","#bbffaa");//索引值为1的<div>元素的背景色
+
+// $("div:parent").css("background","#bbffaa");//选取拥有子元素的<div>元素
+$("div:contains('id')").css("background","#bbffaa");//选取拥有子元素的<div>元素
+
